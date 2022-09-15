1d13de838416b23202962fa2d91548fb440f0724
AlchemicAIO <randy.oneal95@gmail.com>
2022-09-14 20:23:17 -0400

    javaTestvariables

---
+++ src/javaTestvariables/Main.java
@@ -0,0 +1,21 @@
+package javaVariables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		boolean d = true;
+		int a = 1;
+		byte b = -1;
+		short c = 40;
+		int = 5;
+		long = -500000L;
+		double = 1.5;
+		char = f;
+		float = 99.9f;
+		String = "hello world";
+		
+		
+		
+		
+	}
+}
